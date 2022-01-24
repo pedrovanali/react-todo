c58501fe22a781cb53cc079685dc9959dceed464
Pedro Henrique Banali <pedro.banali@gmail.com>
2022-01-23 22:11:16 -0300

    feat(todo-list): add remove functionality to todo list

--- src/components/TodoList.test.tsx
+++ src/components/TodoList.test.tsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { createEvent, fireEvent, render, screen } from "@testing-library/react";
 import { TodoDispatchContext, TodoStateContext } from "../contexts/TodoContext";
+import { deleteTodo, toggleTodo } from "../reducers/todo.reducer/TodoActions";
 import TodoList from "./TodoList";
 
 describe("Todo List test suite", () => {
@@ -52,5 +53,45 @@ describe("Todo List test suite", () => {
     const newTodoItem = screen.getByTestId(0);
     fireEvent.click(newTodoItem);
     expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenCalledWith(toggleTodo(0));
+  });
+  it("should check if delete button exists", () => {
+    const todos = {
+      0: { id: 0, completed: true, name: "new Todo" },
+      1: { id: 1, completed: false, name: "newer Todo" },
+    };
+
+    render(
+      <TodoStateContext.Provider value={{ lastTodoId: 1, todos }}>
+        <TodoList />
+      </TodoStateContext.Provider>
+    );
+    const deleteButton0 = screen.getByTestId("delete-0");
+    const deleteButton1 = screen.getByTestId("delete-1");
+    expect(deleteButton0).toBeInTheDocument();
+    expect(deleteButton1).toBeInTheDocument();
+  });
+  it("should delete todo when clicked on todo delete button", () => {
+    const todos = {
+      0: { id: 0, completed: true, name: "new Todo" },
+      1: { id: 1, completed: false, name: "newer Todo" },
+    };
+    const dispatch = jest.fn();
+    render(
+      <TodoStateContext.Provider value={{ lastTodoId: 1, todos }}>
+        <TodoDispatchContext.Provider value={dispatch}>
+          <TodoList />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
+    );
+    const deleteButton0 = screen.getByTestId("delete-0");
+    const clickEvent = createEvent.click(deleteButton0);
+    clickEvent.preventDefault();
+    clickEvent.stopPropagation();
+    fireEvent.click(deleteButton0, clickEvent);
+
+    expect(clickEvent.defaultPrevented).toBe(true);
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenCalledWith(deleteTodo(0));
   });
 });
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { TodoDispatchContext, TodoStateContext } from "../contexts/TodoContext";
-import { toggleTodo } from "../reducers/todo.reducer/TodoActions";
+import { deleteTodo, toggleTodo } from "../reducers/todo.reducer/TodoActions";
 import { getAllTodos } from "../selectors/todo.selectors/TodoSelectors";
 
 const TodoList = () => {
@@ -10,6 +10,14 @@ const TodoList = () => {
   const todosList = getAllTodos(todos);
   const toggleTodoItem = (id: number) => {
     dispatch(toggleTodo(id));
+  };
+  const deleteTodoItem = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    id: number
+  ) => {
+    e.stopPropagation();
+    e.preventDefault();
+    dispatch(deleteTodo(id));
   };
   return (
     <div>
@@ -23,6 +31,15 @@ const TodoList = () => {
         >
           <span>{todoItem.name}</span>
           <strong> {todoItem.completed ? "YES" : "NO"}</strong>
+          <button
+            type="button"
+            data-testid={`delete-${todoItem.id}`}
+            onClick={(e) => {
+              deleteTodoItem(e, todoItem.id);
+            }}
+          >
+            delete
+          </button>
           <br />
         </div>
       ))}
